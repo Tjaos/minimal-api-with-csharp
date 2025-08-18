375935c51383bc68682fcc95e63a1d8aa23d7982
Tjaos <th83287716@gmail.com>
2025-08-18 18:04:51 -0300

    feat: implementando endpoints de administrador

--- Dominio/Servicos/AdministradorServico.cs
+++ Dominio/Servicos/AdministradorServico.cs
@@ -13,10 +13,36 @@ public class AdministradorServico : IAdministradorServico
     {
         _contexto = contexto;
     }
+
+    public Administrador Incluir(Administrador administrador)
+    {
+        _contexto.Administradores.Add(administrador);
+        _contexto.SaveChanges();
+
+        return administrador;
+    }
+
     public Administrador? Login(LoginDTO loginDTO)
     {
         var adm = _contexto.Administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
         return adm;
     }
+    
+    public Administrador? BuscaPorId(int id)
+    {
+        return _contexto.Administradores.Where(a => a.Id == id).FirstOrDefault();
+    }
 
-}
+    public List<Administrador> Todos(int? pagina)
+    {
+        var query = _contexto.Administradores.AsQueryable();
+
+        int itensPorPagina = 10;
+
+        query = query.Skip(((pagina ?? 1) - 1) * itensPorPagina).Take(itensPorPagina);
+
+        return query.ToList();
+    }
+
+        
+    }
